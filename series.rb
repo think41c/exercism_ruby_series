829d99dd917d7ca81f49acfbc34377999ea9bfa5
Tony DiNitto <think41c@gmail.com>
2015-04-13 13:59:28 -0600

    Slight refactoring.

--- series.rb
+++ series.rb
@@ -1,19 +1,16 @@
 class Series
 
-  def initialize(num_to_slice)
-    @num_to_slice = num_to_slice
+  def initialize(number_slice)
+    @number_slice = number_slice
   end
 
   def slices(group_size)
-    if group_size > @num_to_slice.length 
-      fail ArgumentError, "You put in too large a group to slice by."
-    else
-      each_char  = @num_to_slice.chars
-      each_numb  = each_char.map(&:to_i)
-      each_group = each_numb.each_cons(group_size)
-      grouped    = each_group.to_a
-    end
+    if group_size > @number_slice.length then fail ArgumentError, "Size too large." end
+      
+    each_char  = @number_slice.chars
+    each_numb  = each_char.map(&:to_i)
+    each_group = each_numb.each_cons(group_size)
+    grouped    = each_group.to_a
   grouped
   end
 end
-
